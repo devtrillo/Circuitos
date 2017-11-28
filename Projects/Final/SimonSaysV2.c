6fa0a7338cc42d2659c5eb29163af2caa7556a68
Esteban Trillo <tevan0vihc@gmail.com>
2017-11-28 22:00:51 +0000

    It works

--- Projects/Final/SimonSaysV2.c
+++ Projects/Final/SimonSaysV2.c
@@ -8,7 +8,8 @@ int SwitchNumberInput();
 void ShowSequence();
 
 int inputPins[] = {7, 0, 2, 3};
-int outputPins[] = {29, 28, 27, 25};
+int outputPins[] = {29, 28, 27, 26};
+int powerPins[] = {25, 24, 23, 22};
 int sequence[10] = { 0 };
 
 int main(int argc, char const * argv[])
@@ -28,7 +29,7 @@ int main(int argc, char const * argv[])
 		for (int j = 0; j <= i; ++j)
 		{
 			int  index = SwitchNumberInput();
-			if (index != sequence[i]) {
+			if (index != sequence[j]) {
 				printf("Secuencia incorrecta\n");
 				printf("index = %d | sequence[i] = %d\n", index, sequence[i]);
 				return -1;
@@ -42,32 +43,37 @@ int Initialice() {
 	int inputSize = sizeof(inputPins) / sizeof(*inputPins);
 	if (inputSize != outputSize)
 		return -1;
-	srand(time(NULL));
 	for (int i = 0; i <  inputSize; ++i)
 	{
 		pinMode(inputPins[i], INPUT);
-		pullUpDnControl(inputPins[i], PUD_UP);
+		pullUpDnControl(inputPins[i], PUD_DOWN);
 		printf("Pin %d set as INPUT\n", inputPins[i]);
 	}
 	for (int i = 0; i <  outputSize; ++i) {
 		pinMode(outputPins[i], OUTPUT);
-		digitalWrite(outputPins[i], HIGH);
+		digitalWrite(outputPins[i], LOW);
 		printf("Pin %d set as OUTPUT\n", outputPins[i]);
+	}
+	for (int i = 0; i <  outputSize; ++i) {
+		pinMode(powerPins[i], OUTPUT);
+		digitalWrite(powerPins[i], HIGH);
+		printf("Pin %d set as POWER\n", powerPins[i]);
 	}
 	return 0;
 }
 int SwitchNumberInput() {
 	int index = 999;
 	while (index == 999) {
+
 		for (int i = 0; i <  sizeof(inputPins) / sizeof(*inputPins); ++i)
 		{
-			printf("checking  button %d\n", i);
-			delay(200);
+			delay(10);
+			//printf("checking  button %d\n", i);
 			if (digitalRead(inputPins[i]) == 1) {
-				index = inputPins[i];
-				printf("Pressed Button #%d, Pin #%d\n", i + 1, inputPins[i] );
+				index = i;
+				printf("Pressed Button #%d, Pin #%d\n", i, inputPins[i] );
 				while (digitalRead(inputPins[i]) == 1) {
-					delay(200);
+					delay(10);
 				}
 				break;
 			}
@@ -79,12 +85,11 @@ void ShowSequence(int size) {
 	for (int i = 0; i <= size; ++i)
 	{
 		if (sequence[i] == 0)
-			sequence[i] = i % sizeof(outputPins) / sizeof(*outputPins);
-		//sequence[i] =  sizeof(outputPins) / sizeof(*outputPins);
+			sequence[i] =  rand()%sizeof(outputPins) / sizeof(*outputPins);
 		printf("%d\n", sequence[i]);
-		digitalWrite(outputPins[sequence[i]], LOW);
-		delay(2000);
 		digitalWrite(outputPins[sequence[i]], HIGH);
-
+		delay(2000);
+		digitalWrite(outputPins[sequence[i]], LOW);
+		delay(500);
 	}
 }
--- Projects/Final/test.c
+++ Projects/Final/test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-void Initialice();
+int Initialice();
 int SwitchNumberInput();
 
-int inputPins[] = {7, 0, 1};
+int inputPins[] = {7, 0, 2, 3};
+int outputPins[] = {29, 28, 27, 26};
+int powerPins[] = {25, 24, 23, 22};
 
 int main(int argc, char const *argv[])
 {
@@ -23,30 +25,41 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
-void Initialice() {
-	int i = 0;
-	printf("Number of input pins: %d\n", sizeof(inputPins) / sizeof(*inputPins));
-	while (i < sizeof(inputPins) / sizeof(*inputPins))
+int Initialice() {
+	int outputSize = sizeof(outputPins) / sizeof(*outputPins);
+	int inputSize = sizeof(inputPins) / sizeof(*inputPins);
+	if (inputSize != outputSize)
+		return -1;
+	for (int i = 0; i <  inputSize; ++i)
 	{
 		pinMode(inputPins[i], INPUT);
-		pullUpDnControl(inputPins[i], PUD_UP);
-		printf("input pin %d set as INPUT\n", inputPins[i]);
-		i++;
+		pullUpDnControl(inputPins[i], PUD_DOWN);
+		printf("Pin %d set as INPUT\n", inputPins[i]);
 	}
+	for (int i = 0; i <  outputSize; ++i) {
+		pinMode(outputPins[i], OUTPUT);
+		digitalWrite(outputPins[i], LOW);
+		printf("Pin %d set as OUTPUT\n", outputPins[i]);
+	}
+	for (int i = 0; i <  outputSize; ++i) {
+		pinMode(powerPins[i], OUTPUT);
+		digitalWrite(powerPins[i], HIGH);
+		printf("Pin %d set as POWER\n", powerPins[i]);
+	}
+	return 0;
 }
 int SwitchNumberInput() {
 	int index = 999;
 	while (index == 999) {
-		
+
 		for (int i = 0; i <  sizeof(inputPins) / sizeof(*inputPins); ++i)
 		{
-			delay(50);
+			//printf("checking  button %d\n", i);
 			if (digitalRead(inputPins[i]) == 1) {
 				index = i;
-				printf("Pressed Button #%d, Pin #%d\n", i, inputPins[i] );
+				printf("Pressed Button #%d, Pin #%d\n", i + 1, inputPins[i] );
 				while (digitalRead(inputPins[i]) == 1) {
-					printf("Waiting\n");
-					delay(100);
+					;
 				}
 				break;
 			}
